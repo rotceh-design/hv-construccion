cc516147dfd3225dabbe1bca79f9db2df1c564a3
Hector Valenzuela <hec.valenzuela@duocuc.cl>
2026-05-06 10:58:25 -0400

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Asegura que las rutas se construyan desde la raíz para evitar errores 404 en despliegues
+  base: '/', 
+  build: {
+    // Genera una estructura limpia para el despliegue en Vercel
+    outDir: 'dist',
+  }
 })
